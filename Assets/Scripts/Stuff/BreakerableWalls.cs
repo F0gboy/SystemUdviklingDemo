1c1e6bb133167a6e00e5b49efb31e134ce1f9766
F0gboy <Jasp3r@live.dk>
2024-03-21 13:39:37 +0100

    Sound Update

--- Assets/Scripts/Stuff/BreakerableWalls.cs
+++ Assets/Scripts/Stuff/BreakerableWalls.cs
@@ -8,23 +8,33 @@ public class BreakerableWalls : MonoBehaviour, IDamageable
 {
     [SerializeField]
     int health;
+    
+    [SerializeField]
+    private AudioClip breakSound;
     private AudioSource audioSource;
 
     public void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
+        // Assign the clip to the AudioSource (if you prefer it here)
+        audioSource.clip = breakSound;
     }
 
     public void TakeDamage(int amount)
     {
+        
+        audioSource.clip = breakSound; // Ensure the clip is assigned
+        audioSource.pitch = Random.Range(0.9f, 1.1f); // Adjust these values as needed
+        audioSource.Play();
+        
         health -= amount;
         if (health <= 0) Breake();
     }
 
     private void Breake()
     {
-        audioSource.pitch = Random.Range(0.9f, 1.1f); // Adjust these values as needed
-        audioSource.Play();
+
+        Console.WriteLine("PLAYED");
         
         Destroy(gameObject);
     }
