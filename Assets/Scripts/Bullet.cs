896cdcf39ff550c67434aed7175edc339215072a
RasmusBayThomsen <mikemocals@gmail.com>
2024-03-21 13:36:51 +0100

    bullet optimization

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,53 +5,32 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public ParticleSystem Blood;
+    private AudioSource audioSource;
     Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 4);
-        
-
+        audioSource = GetComponent<AudioSource>();
     }
-
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         IDamageable hitObject = collision.gameObject.GetComponent<IDamageable>();
-        if (hitObject != null) hitObject.TakeDamage(10);
-        
-
-        if (collision.gameObject.layer == 6) 
-        { 
-            Blood.Play(); 
-            gameObject.GetComponent<AudioSource>().Play();
-            Destroy(GetComponent<Rigidbody2D>()); 
-            Destroy(GetComponent<SpriteRenderer>()); 
-            Destroy(GetComponent<BoxCollider2D>());
-            StupidZombie stupidZombie = collision.gameObject.GetComponent<StupidZombie>();
-            stupidZombie.health--;
-            Destroy(gameObject, 1); 
-        }
-
-
-        else
+        if (hitObject != null)
         {
-            if (collision.gameObject.layer == 7) { }
-            else
-            {
-                Destroy(gameObject);
-            }
+            Blood.Play();
+            hitObject.TakeDamage(10);
         }
-        
+
+        audioSource.Play();
+
+        //Disable all components
+        Destroy(GetComponent<Rigidbody2D>());
+        Destroy(GetComponent<SpriteRenderer>());
+        Destroy(GetComponent<BoxCollider2D>());
+
+        //Destroy rest later so audio and particles can play out
+        Destroy(gameObject, 1);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-    }
-
-  
 }
