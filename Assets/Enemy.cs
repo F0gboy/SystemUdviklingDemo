05d63cc156f597fe82df20cd1de7ee33759a871d
RasmusBayThomsen <mikemocals@gmail.com>
2024-03-21 13:13:49 +0100

    Enemy dealing damage to player

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -10,6 +10,7 @@ public interface IDamageable
 
 public class Enemy : MonoBehaviour, IDamageable
 {
+    private float cooldown;
     protected Rigidbody2D rb;
     protected GameObject player;
     private NavMeshAgent agent;
@@ -17,6 +18,7 @@ public class Enemy : MonoBehaviour, IDamageable
     [SerializeField] public bool alive = true;
     [SerializeField] protected int health = 5;
     [SerializeField] protected int damage = 2;
+    [SerializeField] protected float damageRate = 0.5f;
     [SerializeField] protected int detectionRange = 10;
     public int Health { get => health; set { health = value; if (health < 0) health = 0; } } //Sets to 0 if it goes negative
     public int Damage { get => damage; set => damage = value; }
@@ -36,15 +38,23 @@ public class Enemy : MonoBehaviour, IDamageable
     {
         if (alive == false) return;
 
-        //Move towards player if it's within radius
+        //Update target if player is within radius
         Vector3 dir = player.transform.position - transform.position;
         if (dir.magnitude < detectionRange)
             agent.SetDestination(player.transform.position);
 
-        //Update target direction
-        //if (player.GetComponent<PlayerScript>().died == false)
-        //    agent.SetDestination(player.transform.position);
+        //Exists so enemy can't deal damage every frame
+        if (cooldown > 0) cooldown -= Time.deltaTime;
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player && cooldown <= 0)
+        {
+            Debug.Log("Enemy dealt damage to player");
+            player.GetComponent<PlayerScript>().Health -= Damage;
+            cooldown = damageRate;
+        }
     }
 
     public void TakeDamage(int amount)
