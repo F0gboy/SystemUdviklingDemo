da30c6c3564055a7cb520f735b1a20e42d4826dc
RasmusBayThomsen <mikemocals@gmail.com>
2024-03-21 11:02:37 +0100

    enemy behavior (hope it works)

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public interface IDamageable
 {
@@ -11,7 +12,9 @@ public class Enemy : MonoBehaviour, IDamageable
 {
     protected Rigidbody2D rb;
     protected GameObject player;
+    private NavMeshAgent agent;
 
+    [SerializeField] public bool alive = true;
     [SerializeField] protected int health = 5;
     [SerializeField] protected int damage = 2;
     [SerializeField] protected int detectionRange = 10;
@@ -22,13 +25,22 @@ public class Enemy : MonoBehaviour, IDamageable
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((player.transform.position - transform.position).magnitude < 5)
+        //Move towards player if it's within radius
+        //Vector3 dir = player.transform.position - transform.position;
+        //if (dir.magnitude < detectionRange)
+        //    transform.position += dir.normalized * Time.deltaTime;
+
+        //Update target direction
+        if (alive == true && player.GetComponent<PlayerScript>().died == false)
+            agent.SetDestination(player.transform.position);
+
     }
 
     public void TakeDamage(int amount)
@@ -39,6 +51,6 @@ public class Enemy : MonoBehaviour, IDamageable
 
     private void Die()
     {
-
+        Destroy(gameObject);
     }
 }
